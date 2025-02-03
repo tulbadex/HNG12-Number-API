063d0e4caa2277419cb70b0d80f9d81272ce2d95
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2025-02-03 08:44:53 +0000

    fix: fix index file

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($request_uri !== '/api/classify-number') {
     http_response_code(404);
-    echo json_encode(["error" => "Invalid endpoint"]);
+    echo json_encode(["error" => true, "message" => "Invalid endpoint"]);
     exit;
 }
 
@@ -25,35 +25,51 @@ function is_perfect($num) {
     for ($i = 2; $i * $i <= $num; $i++) {
         if ($num % $i == 0) {
             $sum += $i;
-            if ($i != $num / $i) $sum += $num / $i;
+            if ($i !== $num / $i) $sum += $num / $i;
         }
     }
-    return $sum == $num;
+    return $sum === $num;
 }
 
 function is_armstrong($num) {
     $digits = str_split($num);
     $power = count($digits);
     $sum = array_sum(array_map(fn($d) => pow($d, $power), $digits));
-    return $sum == $num;
+    return $sum === $num;
 }
 
 function get_fun_fact($num) {
-    $api_url = "http://numbersapi.com/{$num}/math";
-    $fact = @file_get_contents($api_url);
-    return $fact ?: "Fun fact not available.";
+    $api_url = "http://numbersapi.com/{$num}/math?json";
+    
+    $context = stream_context_create([
+        "http" => ["ignore_errors" => true] // Handle failures properly
+    ]);
+
+    $response = @file_get_contents($api_url, false, $context);
+
+    if ($response === false) {
+        return "Fun fact not available.";
+    }
+
+    $data = json_decode($response, true);
+    return $data['text'] ?? "Fun fact not available.";
 }
+
 // Validate input
 if (!isset($_GET['number']) || !ctype_digit($_GET['number'])) {
-    echo json_encode(["number" => $_GET['number'] ?? "undefined", "error" => true]);
     http_response_code(400);
+    echo json_encode([
+        "error" => true,
+        "number" => $_GET['number'] ?? null
+    ]);
     exit;
 }
 
 $number = intval($_GET['number']);
 $properties = [];
+
 if (is_armstrong($number)) $properties[] = "armstrong";
-$properties[] = ($number % 2 == 0) ? "even" : "odd";
+$properties[] = ($number % 2 === 0) ? "even" : "odd";
 
 $response = [
     "number" => $number,
@@ -61,8 +77,10 @@ $response = [
     "is_perfect" => is_perfect($number),
     "properties" => $properties,
     "digit_sum" => array_sum(str_split($number)),
-    "fun_fact" => get_fun_fact($number)
+    "fun_fact" => get_fun_fact($number),
+    "error" => false
 ];
 
-echo json_encode($response);
+http_response_code(200);
+echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 ?>
